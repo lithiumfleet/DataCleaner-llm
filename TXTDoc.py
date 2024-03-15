8208e832e26a755b223485a4b5e5b5f0ef7d3223
lithiumfleet <2760706819@qq.com>
2024-03-15 03:05:25 +0000

    save to file

--- TXTDoc.py
+++ TXTDoc.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 
 
@@ -6,18 +7,15 @@ class TXTDoc:
     def __init__(self) -> None:
         self.fp = None
 
-    def open_file(self, file_path:str):
+    def open_file(self, file_path:str, encoding='utf8'):
         assert os.access(file_path, os.R_OK&os.F_OK),"[DEBUG] "+file_path+" is not avaliable!"
-        self.fp = open(file_path, 'r', encoding="gbk")
+        self.fp = open(file_path, 'r', encoding=encoding)
+
 
     def get_chunck(self, chunck_size:int):
         assert self.fp != None and not self.fp.closed, "[DEBUG] "+self.fp.name+" is not avaliable!"
         return self.fp.read(chunck_size)
 
-    def check_eof(self) -> bool:
-        if self.fp.tell()==0:
-            self.fp.close()
-            return True
-        else:
-            return False
-
+    def close_file(self) -> bool:
+        logging.info("finish clean {}".format(self.fp.name))
+        self.fp.close()
--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import json
 from TXTDoc import TXTDoc
+import os
+from tqdm import tqdm
 
 
 # get config
@@ -12,11 +14,13 @@ few_shot_examples = config['few_shot_examples']
 base_url = config['base_url']
 api_key = config['api_key']
 test_file_path = config['test_file_path']
+output_dir = config['output_dir']
+chunck_size = config['chunck_size']
+file_encoding = config['file_encoding']
 
 
 # create client
 client = OpenAI(base_url=base_url, api_key=api_key)
-
 
 # make message with history
 message = [
@@ -28,22 +32,31 @@ for index, example in enumerate(few_shot_examples):
   message.append({"role": "assistant", "content": example['output']})
 
 
-
+# start clean
 txtDoc = TXTDoc()
-txtDoc.open_file(test_file_path)
-chunck = " "
+txtDoc.open_file(test_file_path, file_encoding)
 
-while chunck != "":
+with open(output_dir+os.sep+txtDoc.fp.name, 'w') as output_fp:
+  with tqdm(total=len(txtDoc.fp.read())) as qbar:
+    txtDoc.fp.seek(0)
+    while True:
 
-  chunck = txtDoc.get_chunck(1024)
+      chunck = txtDoc.get_chunck(chunck_size=chunck_size)
+      if not chunck:
+        txtDoc.close_file()
+        break
 
-  message.append({"role": "user", "content": chunck})
+      message.append({"role": "user", "content": chunck})
 
-  completion = client.chat.completions.create(
-    model = "Baichuan2-13B-Chat",
-    messages = message
-  )
+      completion = client.chat.completions.create(
+        model = "Baichuan2-13B-Chat",
+        messages = message,
+        temperature = 0.8
+      )
 
-  print(completion.choices[0].message.content)
+      output_fp.write(completion.choices[0].message.content.strip())
+      output_fp.flush()
 
-  message.pop()
+      message.pop()
+      qbar.update(chunck_size)
+      
